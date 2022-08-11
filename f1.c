e39415bb0def10686c0daa0cc56d9f3c97b10c16
nisim <nisimsharma51@gmail.com>
2022-08-11 11:23:42 +0545

    alg1

---
+++ f1.c
@@ -0,0 +1,12 @@
+// Read wordlist and return random word 
+char * word(char * filename)
+{
+    // File Reader
+    // Save to Array
+    // Generate Random Index
+    // Return Random Word
+}
+
+// Display Hangman state
+
+
