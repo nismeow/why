313673341347c70d86f19201fc9c94bb17957103
nisim <nisimsharma51@gmail.com>
2022-08-15 10:12:31 +0545

    draw platform

--- f1.c
+++ f1.c
@@ -1,12 +1,69 @@
-// Read wordlist and return random word 
-char * word(char * filename)
+#include <stdio.h>
+int main()
 {
-    // File Reader
-    // Save to Array
-    // Generate Random Index
-    // Return Random Word
-}
+    
+        int i;
+
+// Read wordlist and return random word 
+char * word(char * filename);
 
 // Display Hangman state
 
+// void draw()
+// {
+//     //draws one each time a guess is incorrect
+    char *stage[]= {
+        
+                    
 
+                     "  |=====|\n"
+                     "        |\n"
+                     "        |\n"
+                     "        |\n"
+                     "       ===\n",
+
+                     "  |=====|\n"
+                     "  O     |\n"
+                     "        |\n"
+                     "        |\n"
+                     "       ===\n",
+
+                     "  |=====|\n"
+                     "  O     |\n"
+                     "  |     |\n"
+                     "        |\n"
+                     "       ===\n",
+
+                     "  |=====|\n"
+                     "  O     |\n"
+                     "  |-    |\n"
+                     "        |\n"
+                     "       ===\n",
+
+                     "  |=====|\n"
+                     "  O     |\n"
+                     " -|-    |\n"
+                     "        |\n"
+                     "       ===\n",
+
+                     "  |=====|\n"
+                     "  O     |\n"
+                     " -|-    |\n"
+                     "  /     |\n"
+                     "       ===\n",
+
+                     "   |=====|\n"
+                     "   O     |\n"
+                     "  -|-    |\n"
+                     "  //     |\n"
+                     "       ===\n"
+  
+    };
+
+       for(i=0; i<7; i++)
+    {
+     printf("%s\n", stage[i]);
+    }
+   
+  
+}
